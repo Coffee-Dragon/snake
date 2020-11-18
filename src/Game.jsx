9723eb173ec0c63ff4993f77a72589d08ca13143
Coffee-Dragon <alina.offic@gmail.com>
2020-11-18 03:53:07 +0100

    Made snake move

--- src/Game.jsx
+++ src/Game.jsx
@@ -7,11 +7,25 @@ export class Game extends React.PureComponent {
   state = {
     snakeSegments: [[5, 4], [4, 4]],
   };
-
+  /** @enum {'right'|'left'|'up'|'down'} */
+  direction = 'right';
+  intervalID;
+  nextStep() {
+    let snakeSegments = [...this.state.snakeSegments];
+    snakeSegments.unshift([this.state.snakeSegments[0][0]+1, this.state.snakeSegments[0][1]]);
+    snakeSegments.pop();
+    this.setState({snakeSegments: snakeSegments});
+  }
+  start() {
+    this.intervalID = setInterval(this.nextStep.bind(this), 1000);
+  }
+  componentWillUnmount() {
+    clearInterval(this.intervalID);
+  }
   render() {
     return <div>
       <h1>Snake is on</h1>
-      <button>Start</button>
+      <button onClick={this.start.bind(this)}>Start</button>
       <Field>
         <Snake segments={this.state.snakeSegments} />
       </Field>
